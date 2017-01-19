a0c259d73c870201a0e79bef4bb0b589f752c141
Luke Nigro <lnigro903@g.rwu.edu>
2017-01-19 02:57:31 +0000

    Added code comments

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,17 +1,34 @@
 class ContactsController < ApplicationController
+  
+  #Sends a GET request to /contact-us
+  #Shows a new contact form
   def new
     @contact = Contact.new
   end
   
+  #Sends a POST request to /contacts
   def create
+    #Mass assignment of form fields into contact objects
     @contact = Contact.new(contact_params)
+    
+    #Saves the contact object to the database
     if @contact.save
+      
+      #Store the form fields to local variables.
       name = params[:contact][:name]
       email = params[:contact][:email] 
       body = params[:contact][:comments]
+      
+      #Plug variables into the contact_mailer method
       ContactMailer.contact_email(name, email, body).deliver
+      
+      #Store the success message into flash hash
+      #Redirect back to /contact-us
       flash[:success] = 'Message Sent.'
       redirect_to new_contact_path
+    
+    #If the save is unsuccessful, this stores the errors to the
+    #flash hash as well, and redirects to /contact-us
     else
       flash[:danger] = @contact.errors.full_messages.join(', ')
       redirect_to new_contact_path
@@ -19,6 +36,8 @@ class ContactsController < ApplicationController
   end
   
   private
+    #Collects data from forms using strong parameters
+    #Whitelists the form fields.
     def contact_params
       params.require(:contact).permit(:name, :email, :comments)
     end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,17 @@
 class ContactMailer < ActionMailer::Base
+  
+  #Sets the default email to send to
   default to: 'lnigro903@g.rwu.edu'
   
+  
   def contact_email(name, email, body)
+    
+    #Create instance variables for each email parameter
     @name = name
     @email = email
     @body = body
     
+    #Sets the from address and the subject line of the email to be sent
     mail(from: email, subject: "Contact form message from #{name}")
   end
 end
